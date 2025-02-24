b493040f6e287c98879a9bf0b9a57718ea87731b
szczurek9 <maciejmyszk88@wp.pl>
2025-02-24 21:27:00 +0100

    Update kreator_poziomow.bat

--- LoW_12_Patch_2/LoW 1.2/game/levels/kreator_poziomow.bat
+++ LoW_12_Patch_2/LoW 1.2/game/levels/kreator_poziomow.bat
@@ -3,14 +3,12 @@ setlocal enabledelayedexpansion
 title LoW Level Creator v1.0
 cls
 
-:: Pobranie numeru poziomu
 echo.
 echo LoW Level Creator version 1.0 - Copyright @ 2025 szczurek9
 echo.
 set /p level_number=Podaj numer poziomu ktory chcesz utworzyc (numer musi byc inny niz istniejacy):
 echo.
 
-:: Sprawdzenie, czy plik już istnieje
 set "level_file=level%level_number%.txt"
 if exist "%level_file%" (
     echo Blad: Plik %level_file% juz istnieje!
@@ -18,7 +16,6 @@ if exist "%level_file%" (
     exit
 )
 
-:: Pobieranie danych od uzytkownika
 set /p enemy_name=Podaj nazwe przeciwnika:
 set /p enemy_hp=Podaj HP przeciwnika:
 set /p enemy_dmg=Podaj DMG przeciwnika:
@@ -28,7 +25,6 @@ set /p reward_money=Podaj ilosc hajsu za pokonanie przeciwnika:
 set /p wait_hp_bonus=Podaj ilosc HP odzyskiwana za przeczekanie tury:
 set /p lvl_up=Podaj wiadomosc jaka gracz ma otrzymac na koncu poziomu (aby byla pusta nacisnij enter):
 
-:: Zapis do pliku
 (
     echo enemy_name=%enemy_name%
     echo enemy_hp=%enemy_hp%
@@ -40,7 +36,6 @@ set /p lvl_up=Podaj wiadomosc jaka gracz ma otrzymac na koncu poziomu (aby byla 
     echo lvl_up=%lvl_up%
 ) > "%level_file%"
 
-:: Sprawdzenie, czy plik faktycznie został utworzony
 if exist "%level_file%" (
     cls
     echo.
